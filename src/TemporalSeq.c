a0c7396e6ada6cbd492b1590044e222e748c961a
Esteban Zimanyi <wit@cs.ulb.ac.be>
2019-04-28 22:31:24 +0200

    Fix temporalseq_eq

--- src/TemporalSeq.c
+++ src/TemporalSeq.c
@@ -3470,7 +3470,7 @@ temporalseq_eq(TemporalSeq *seq1, TemporalSeq *seq2)
 		return false;
 
 	/* If bounding boxes are not equal */
-	size_t bboxsize = double_pad(temporal_bbox_size(seq1->valuetypid));
+	size_t bboxsize = temporal_bbox_size(seq1->valuetypid);
 	void *box1 = temporalseq_bbox_ptr(seq1);
 	void *box2 = temporalseq_bbox_ptr(seq2);
 	if (memcmp(box1, box2, bboxsize) != 0)
