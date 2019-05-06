aa449b36dd5feac00779a761365edb187e94f60c
alesuiss <alesuiss@ulb.ac.be>
2019-05-06 13:25:51 +0200

    rounded distance test for temporal geography

--- point/test/queries/62_TempDistance_tbl.test.sql
+++ point/test/queries/62_TempDistance_tbl.test.sql
@@ -7,11 +7,11 @@ WHERE temp <-> g IS NOT NULL LIMIT 10;
 SELECT t1.temp <-> t2.temp FROM tbl_tgeompoint t1, tbl_tgeompoint t2
 WHERE t1.temp <-> t2.temp IS NOT NULL LIMIT 10;
 
-SELECT g <-> temp FROM tbl_geogpoint, tbl_tgeogpoint
+SELECT round(g <-> temp, 6) FROM tbl_geogpoint, tbl_tgeogpoint
 WHERE g <-> temp IS NOT NULL LIMIT 10;
-SELECT temp <-> g FROM tbl_tgeogpoint, tbl_geogpoint
+SELECT round(temp <-> g, 6) FROM tbl_tgeogpoint, tbl_geogpoint
 WHERE temp <-> g IS NOT NULL LIMIT 10;
-SELECT t1.temp <-> t2.temp FROM tbl_tgeogpoint t1, tbl_tgeogpoint t2
+SELECT round(t1.temp <-> t2.temp, 6) FROM tbl_tgeogpoint t1, tbl_tgeogpoint t2
 WHERE t1.temp <-> t2.temp IS NOT NULL LIMIT 10;
 
 -------------------------------------------------------------------------------
@@ -23,11 +23,11 @@ WHERE temp <-> g IS NOT NULL LIMIT 10;
 SELECT t1.temp <-> t2.temp FROM tbl_tgeompoint3D t1, tbl_tgeompoint3D t2
 WHERE t1.temp <-> t2.temp IS NOT NULL LIMIT 10;
 
-SELECT g <-> temp FROM tbl_geogpoint3D, tbl_tgeogpoint3D
+SELECT round(g <-> temp, 6) FROM tbl_geogpoint3D, tbl_tgeogpoint3D
 WHERE g <-> temp IS NOT NULL LIMIT 10; 
-SELECT temp <-> g FROM tbl_tgeogpoint3D, tbl_geogpoint3D
+SELECT round(temp <-> g, 6) FROM tbl_tgeogpoint3D, tbl_geogpoint3D
 WHERE temp <-> g IS NOT NULL LIMIT 10;
-SELECT t1.temp <-> t2.temp FROM tbl_tgeogpoint3D t1, tbl_tgeogpoint3D t2
+SELECT round(t1.temp <-> t2.temp, 6) FROM tbl_tgeogpoint3D t1, tbl_tgeogpoint3D t2
 WHERE t1.temp <-> t2.temp IS NOT NULL LIMIT 10;
 
 -------------------------------------------------------------------------------
