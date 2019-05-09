70baa2b97bc9c317562688a92cc3dd2cc21803ab
Esteban Zimanyi <wit@cs.ulb.ac.be>
2019-05-09 13:48:15 +0200

    Fix regression tests

--- test/queries/32_BoundBoxOps_tbl.test.sql
+++ test/queries/32_BoundBoxOps_tbl.test.sql
@@ -1788,4 +1788,11 @@ SELECT * FROM test_boundboxops
 WHERE noidx <> gistidx or noidx <> spgistidx or gistidx <> spgistidx
 ORDER BY op, leftarg, rightarg;
 
+DROP INDEX tbl_tbool_spgist_idx;
+DROP INDEX tbl_tint_spgist_idx;
+DROP INDEX tbl_tfloat_spgist_idx;
+DROP INDEX tbl_ttext_spgist_idx;
+
+DROP TABLE test_boundboxops;
+
 /*****************************************************************************/
