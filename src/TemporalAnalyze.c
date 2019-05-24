aa4d36d180b4ba099585f77706fe38a743712b17
mbakli <mohamed_bakli@aun.edu.eg>
2019-05-24 20:27:04 +0200

    Adding the selectivity functions

--- src/TemporalAnalyze.c
+++ src/TemporalAnalyze.c
@@ -3,6 +3,8 @@
  * TemporalAnalyze.c
  *	  Functions for gathering statistics from temporal columns
  *
+ * Portions Copyright (c) 2019, Esteban Zimanyi,
+ * 		Universite Libre de Bruxelles
  * Portions Copyright (c) 1996-2019, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
@@ -15,6 +17,25 @@
 PG_FUNCTION_INFO_V1(temporal_analyze);
 Datum
 temporal_analyze(PG_FUNCTION_ARGS)
+{
+	VacAttrStats *stats = (VacAttrStats *) PG_GETARG_POINTER(0);
+	Datum result = 0;   /* keep compiler quiet */
+	int type = TYPMOD_GET_DURATION(stats->attrtypmod);
+	assert(type == TEMPORAL || type == TEMPORALINST || type == TEMPORALI ||
+		   type == TEMPORALSEQ || type == TEMPORALS);
+	if (type == TEMPORALINST)
+		result = temporalinst_analyze(stats);
+	else if (type == TEMPORALI)
+		result = temporali_analyze(stats);
+	else if(type == TEMPORALSEQ || type == TEMPORALS ||
+			type == TEMPORAL)
+		result = temporal_traj_analyze(stats);
+	return result;
+}
+
+PG_FUNCTION_INFO_V1(tnumber_analyze);
+Datum
+tnumber_analyze(PG_FUNCTION_ARGS)
 {
 	VacAttrStats *stats = (VacAttrStats *) PG_GETARG_POINTER(0);
 	Datum result = 0;   /* keep compiler quiet */
