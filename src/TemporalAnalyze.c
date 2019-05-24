a7a5b445ebfe894e5a6a371f367b9277af3d358c
mbakli <mohamed_bakli@aun.edu.eg>
2019-05-24 16:19:24 +0200

    test

--- src/TemporalAnalyze.c
+++ src/TemporalAnalyze.c
@@ -5,8 +5,6 @@
  *
  * Portions Copyright (c) 1996-2019, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
- * * IDENTIFICATION
- *	include/TemporalAnalyze.c
  *
  *****************************************************************************/
 #include <TemporalTypes.h>
@@ -18,7 +16,18 @@ PG_FUNCTION_INFO_V1(temporal_analyze);
 Datum
 temporal_analyze(PG_FUNCTION_ARGS)
 {
-	PG_RETURN_BOOL(true);
+	VacAttrStats *stats = (VacAttrStats *) PG_GETARG_POINTER(0);
+	Datum result = 0;   /* keep compiler quiet */
+	int type = TYPMOD_GET_DURATION(stats->attrtypmod);
+	temporal_duration_is_valid(type);
+	if (type == TEMPORALINST)
+		result = temporalinst_analyze(stats);
+	else if (type == TEMPORALI)
+		result = temporali_analyze(stats);
+	else if(type == TEMPORALSEQ || type == TEMPORALS ||
+			type == TEMPORAL)
+		result = temporal_traj_analyze(stats);
+	return result;
 }
 
 PG_FUNCTION_INFO_V1(tnumber_analyze);
