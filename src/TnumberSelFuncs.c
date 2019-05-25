a02184e3fe21b8110875cef488b3df0e1ee014e0
mbakli <mohamed_bakli@aun.edu.eg>
2019-05-25 16:11:11 +0200

    Seperating Time and Temporal Selectivity Functions

--- src/TnumberSelFuncs.c
+++ src/TnumberSelFuncs.c
@@ -1,14 +1,15 @@
 /*****************************************************************************
  *
- * TempSelFuncs.c
+ * TnumberSelFuncs.c
  *	  Functions for selectivity estimation of operators on temporal types
  *
- * Portions Copyright (c) 2019, Esteban Zimanyi, Arthur Lesuisse,
+ * Portions Copyright (c) 2019, Esteban Zimanyi, Mahmoud Sakr, Mohamed Bakli,
  * 		Universite Libre de Bruxelles
  * Portions Copyright (c) 1996-2019, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- *	These functions are only stubs, they need to be written TODO
+ * IDENTIFICATION
+ *	src/TnumberSelFuncs.c
  *
  *****************************************************************************/
  
