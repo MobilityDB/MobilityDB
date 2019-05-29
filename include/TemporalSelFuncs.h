d2308b4605bdfb04c57bc0fe848defdc0cf4a94b
Esteban Zimanyi <wit@cs.ulb.ac.be>
2019-05-29 13:38:09 +0200

    Cleaning selectivity functions for time types

--- include/TemporalSelFuncs.h
+++ include/TemporalSelFuncs.h
@@ -51,28 +51,33 @@ extern double bbox_contained_sel_internal(PlannerInfo *root, VariableStatData va
 extern double bbox_same_sel_internal(PlannerInfo *root, VariableStatData vardata, ConstantData constantData);
 
 /*****************************************************************************
- * Helper functions for calculating selectivity of period type.
+ * Helper functions for calculating selectivity of time types.
  *****************************************************************************/
+
 extern double default_period_selectivity(Oid operator);
-extern int period_rbound_bsearch(PeriodBound *value, PeriodBound *hist, int hist_length, bool equal);
-extern float8 get_period_position(PeriodBound *value, PeriodBound *hist1, PeriodBound *hist2);
+extern int period_rbound_bsearch(PeriodBound *value, PeriodBound *hist, 
+	int hist_length, bool equal);
+extern float8 get_period_position(PeriodBound *value, PeriodBound *hist1, 
+	PeriodBound *hist2);
 extern float8 get_len_position(double value, double hist1, double hist2);
 extern float8 get_period_distance(PeriodBound *bound1, PeriodBound *bound2);
-extern int length_hist_bsearch(Datum *length_hist_values, int length_hist_nvalues, double value, bool equal);
+extern int length_hist_bsearch(Datum *length_hist_values, 
+	int length_hist_nvalues, double value, bool equal);
 extern double calc_period_hist_selectivity(VariableStatData *vardata,
-										   Period *constval, Oid operator, StatisticsStrategy strategy);
+	Period *constval, Oid operator);
 extern double calc_period_hist_selectivity_scalar(PeriodBound *constbound,
-												  PeriodBound *hist, int hist_nvalues, bool equal);
+	PeriodBound *hist, int hist_nvalues, bool equal);
 extern double calc_length_hist_frac(Datum *length_hist_values,
- 	 	 	 	 	 	 	 	 	int length_hist_nvalues, double length1, double length2, bool equal);
+ 	int length_hist_nvalues, double length1, double length2, bool equal);
 extern double calc_period_hist_selectivity_contained(PeriodBound *lower,
- 	 	 	 	 	 	 	 	 	 	 	 	 	 PeriodBound *upper,	PeriodBound *hist_lower, int hist_nvalues,
- 	 	 	 	 	 	 	 	 	 	 	 	 	 Datum *length_hist_values, int length_hist_nvalues);
+ 	PeriodBound *upper, PeriodBound *hist_lower, int hist_nvalues,
+	Datum *length_hist_values, int length_hist_nvalues);
 extern double calc_period_hist_selectivity_contains(PeriodBound *lower,
- 	 	 	 	 	 	 	 	 	 	 	 	 	PeriodBound *upper,	PeriodBound *hist_lower, int hist_nvalues,
- 	 	 	 	 	 	 	 	 	 	 	 	 	Datum *length_hist_values, int length_hist_nvalues);
-extern double calc_period_hist_selectivity_adjacent(PeriodBound *lower, PeriodBound *upper,
-												  PeriodBound *hist_lower, PeriodBound *hist_upper, int hist_nvalues);
+ 	PeriodBound *upper,	PeriodBound *hist_lower, int hist_nvalues,
+ 	Datum *length_hist_values, int length_hist_nvalues);
+extern double calc_period_hist_selectivity_adjacent(PeriodBound *lower, 
+	PeriodBound *upper, PeriodBound *hist_lower, PeriodBound *hist_upper, 
+	int hist_nvalues);
 
 /*****************************************************************************
  * Some other helper functions.
