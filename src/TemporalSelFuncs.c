c4f2c38ffe36e18216441e169e53af83e06a0124
mbakli <mohamed_bakli@aun.edu.eg>
2019-05-29 10:48:49 +0200

    Temporal selectivity functions and merging with the master

--- src/TemporalSelFuncs.c
+++ src/TemporalSelFuncs.c
@@ -99,7 +99,16 @@ PG_FUNCTION_INFO_V1(temporal_same_sel);
 PGDLLEXPORT Datum
 temporal_same_sel(PG_FUNCTION_ARGS)
 {
-	PG_RETURN_FLOAT8(0.001);
+    PlannerInfo *root = (PlannerInfo *) PG_GETARG_POINTER(0);
+    Oid operator = PG_GETARG_OID(1);
+    List *args = (List *) PG_GETARG_POINTER(2);
+    int varRelid = PG_GETARG_INT32(3);
+    Selectivity	selec = temporal_bbox_sel(root, operator, args, varRelid, SAME_OP);
+    if (selec < 0.0)
+        selec = 0.001;
+    else if (selec > 1.0)
+        selec = 1.0;
+    PG_RETURN_FLOAT8(selec);
 }
 
 PG_FUNCTION_INFO_V1(temporal_same_joinsel);
