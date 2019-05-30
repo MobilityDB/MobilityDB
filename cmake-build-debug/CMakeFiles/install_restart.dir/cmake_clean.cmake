c70f536ed038e0c3e96fa5059c8d8219762ca249
mbakli <mohamed_bakli@aun.edu.eg>
2019-05-30 12:36:19 +0200

    General modifications

--- cmake-build-debug/CMakeFiles/install_restart.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install_restart"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install_restart.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/sqlscript.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sqlscript"
-  "mobilitydb--0.0.1.sql"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sqlscript.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
