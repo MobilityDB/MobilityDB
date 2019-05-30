8283e4f42b0d07d8fe42e30d3fb127684c60ef19
mbakli <mohamed_bakli@aun.edu.eg>
2019-05-30 21:13:21 +0200

    Declare range_oid_from_base in temporaltypes.h

--- src/TemporalAnalyze.c
+++ src/TemporalAnalyze.c
@@ -12,8 +12,7 @@
  *	src/TemporalAnalyze.c
  *
  *****************************************************************************/
-#include <TemporalTypes.h>
-#include <TemporalAnalyze.h>
+#include "TemporalAnalyze.h"
 /*****************************************************************************/
 
 
@@ -2700,12 +2699,12 @@ compute_twodim_traj_stats(VacAttrStats *stats, AnalyzeAttrFetchFunc fetchfunc,
 	PeriodBound *temporal_lowers,
 			*temporal_uppers;
 	double total_width = 0;
-	Oid valueRangeType = 0;
+	Oid valueRangeType;
 
 	TemporalArrayAnalyzeExtraData *extra_data = (TemporalArrayAnalyzeExtraData *)stats->extra_data;
 
 	valueRangeType = range_oid_from_base(extra_data->value_type_id);
-
+	
 	value_lowers = (RangeBound *) palloc(sizeof(RangeBound) * samplerows);
 	value_uppers = (RangeBound *) palloc(sizeof(RangeBound) * samplerows);
 	value_lengths = (float8 *) palloc(sizeof(float8) * samplerows);
