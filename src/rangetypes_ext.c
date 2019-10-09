855d3f7ae51d5952e3c9c652e11f8a83f7688e1f
Esteban Zimanyi <wit@cs.ulb.ac.be>
2019-10-09 18:28:25 +0200

    Compatibility with PG10

--- src/rangetypes_ext.c
+++ src/rangetypes_ext.c
@@ -80,7 +80,7 @@ range_make(Datum from, Datum to, bool lower_inc, bool upper_inc, Oid basetypid)
 	else if (basetypid == FLOAT8OID)
 		rangetypid = type_oid(T_FLOATRANGE);
 	else if (basetypid == TIMESTAMPTZOID)
-		rangetypid = TSTZRANGEOID;
+		rangetypid = type_oid(T_TSTZRANGE);
 	
 	TypeCacheEntry* typcache = lookup_type_cache(rangetypid, TYPECACHE_RANGE_INFO);
 
