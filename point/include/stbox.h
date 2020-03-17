db74913997ced76126caa6b846e5eafbf435482b
Esteban Zimanyi <wit@cs.ulb.ac.be>
2020-03-17 16:42:20 +0100

    Compatability with pg10

--- point/include/stbox.h
+++ point/include/stbox.h
@@ -15,6 +15,9 @@
 
 #include <postgres.h>
 #include <catalog/pg_type.h>
+#if MOBDB_PGSQL_VERSION < 110000
+#include <utils/timestamp.h>
+#endif
 
 /*****************************************************************************
  * Struct definition
