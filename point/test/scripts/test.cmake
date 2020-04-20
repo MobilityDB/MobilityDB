276640f1c966bdf54af7f0d63b33d55cd2d40a83
alesuiss <alesuiss@ulb.ac.be>
2020-04-20 17:48:06 +0200

    version-dependent tests

--- point/test/scripts/test.cmake
+++ point/test/scripts/test.cmake
@@ -10,12 +10,21 @@ set_tests_properties(load_geom_tables PROPERTIES DEPENDS create_extension)
 file(GLOB geom_testfiles "point/test/queries/*.sql")
 foreach(file ${geom_testfiles})
 	get_filename_component(TESTNAME ${file} NAME_WE)
-	add_test(
-		NAME ${TESTNAME} 
-		WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/test
-		COMMAND ${PROJECT_SOURCE_DIR}/test/scripts/test.sh run_compare ${CMAKE_BINARY_DIR} ${TESTNAME} ${file} 
-	)
-	set_tests_properties(${TESTNAME} PROPERTIES FIXTURES_REQUIRED DB)
-	set_tests_properties(${TESTNAME} PROPERTIES RESOURCE_LOCK DBLOCK)
+	set(DOTEST TRUE)
+	if(${TESTNAME} MATCHES "_pg([0-9]+)_")
+		if(${PG_MAJOR_VERSION} LESS ${CMAKE_MATCH_1})
+			message("Disabling test ${TESTNAME}")
+			set(DOTEST FALSE)
+		endif()
+	endif()
+	if(DOTEST)
+		add_test(
+			NAME ${TESTNAME} 
+			WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/test
+			COMMAND ${PROJECT_SOURCE_DIR}/test/scripts/test.sh run_compare ${CMAKE_BINARY_DIR} ${TESTNAME} ${file} 
+		)
+		set_tests_properties(${TESTNAME} PROPERTIES FIXTURES_REQUIRED DB)
+		set_tests_properties(${TESTNAME} PROPERTIES RESOURCE_LOCK DBLOCK)
+	endif()
 endforeach()
 
--- test/scripts/test.cmake
+++ test/scripts/test.cmake
@@ -36,14 +36,24 @@ set_tests_properties(teardown PROPERTIES FIXTURES_CLEANUP DB)
 set_tests_properties(teardown PROPERTIES RESOURCE_LOCK DBLOCK)
 
 file(GLOB testfiles "test/queries/*.sql")
+
 foreach(file ${testfiles})
 	get_filename_component(TESTNAME ${file} NAME_WE)
-	add_test(
-		NAME ${TESTNAME} 
-		WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/test
-		COMMAND ${PROJECT_SOURCE_DIR}/test/scripts/test.sh run_compare ${CMAKE_BINARY_DIR} ${TESTNAME} ${file} 
-	)
-	set_tests_properties(${TESTNAME} PROPERTIES FIXTURES_REQUIRED DB)
-	set_tests_properties(${TESTNAME} PROPERTIES RESOURCE_LOCK DBLOCK)
+	set(DOTEST TRUE)
+	if(${TESTNAME} MATCHES "_pg([0-9]+)_")
+		if(${PG_MAJOR_VERSION} LESS ${CMAKE_MATCH_1})
+			message("Disabling test ${TESTNAME}")
+			set(DOTEST FALSE)
+		endif()
+	endif()
+	if(DOTEST)
+		add_test(
+			NAME ${TESTNAME} 
+			WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/test
+			COMMAND ${PROJECT_SOURCE_DIR}/test/scripts/test.sh run_compare ${CMAKE_BINARY_DIR} ${TESTNAME} ${file} 
+		)
+		set_tests_properties(${TESTNAME} PROPERTIES FIXTURES_REQUIRED DB)
+		set_tests_properties(${TESTNAME} PROPERTIES RESOURCE_LOCK DBLOCK)
+	endif()
 endforeach()
 
