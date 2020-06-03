c5e32427eb067b1d2406ebca27f68e768d5da02c
Esteban Zimanyi <wit@cs.ulb.ac.be>
2020-06-03 12:53:05 +0200

    Data generator

--- BerlinMOD/brussels_creategraph.sql
+++ BerlinMOD/brussels_creategraph.sql
@@ -46,18 +46,34 @@ osm2pgsql --create --database brussels --host localhost brussels.osm
 		'services' id='116' priority='4' maxspeed='20' category='3'
 */
 
-DROP TABLE IF EXISTS roads;
-CREATE TABLE roads AS
+CREATE TABLE RoadTypes(id int PRIMARY KEY, type text, priority float, maxspeed float, category int);
+INSERT INTO RoadTypes VALUES
+(101, 'motorway', 1.0, 120, 1),
+(102, 'motorway_link', 1.0, 120, 1),
+(103, 'motorway_junction', 1.0, 120, 1),
+(104, 'trunk', 1.05, 120, 1),
+(105, 'trunk_link', 1.05, 120, 1),
+(106, 'primary', 1.15, 90, 2),
+(107, 'primary_link', 1.15, 90, 1),
+(108, 'secondary', 1.5, 70, 2),
+(109, 'secondary_link', 1.5, 70, 2),
+(110, 'tertiary', 1.75, 50, 2),
+(111, 'tertiary_link', 1.75, 50, 2),
+(112, 'residential', 2.5, 30, 3),
+(113, 'living_street', 3.0, 20, 3),
+(114, 'unclassified', 3.0, 20, 3),
+(115, 'service', 4.0, 20, 3),
+(116, 'services', 4.0, 20, 3);
+
+DROP TABLE IF EXISTS Roads;
+CREATE TABLE Roads AS
 SELECT osm_id, admin_level, bridge, cutting, highway, junction, name,
 	oneway, operator, ref, route, surface, toll, tracktype, tunnel, width,
 	way AS geom
 FROM planet_osm_line
-WHERE highway = 'motorway' OR highway = 'motorway_link' OR highway = 'motorway_junction' OR
-	highway = 'trunk' OR highway = 'trunk_link' OR highway = 'primary' OR
-	highway = 'primary_link' OR highway = 'secondary' OR highway = 'secondary_link' OR
-	highway = 'tertiary' OR highway = 'tertiary_link' OR highway = 'residential' OR
-	highway = 'living_street' OR highway = 'unclassified' OR
-	highway = 'service' OR highway = 'services';
+WHERE highway IN (SELECT type FROM RoadTypes);
+-- SELECT 36757
+-- Query returned successfully in 361 msec.
 
 CREATE INDEX roads_geom_idx ON roads USING GiST(geom);
 
@@ -75,7 +91,7 @@ Temp2 AS (
 )
 SELECT row_number() over (), geom
 FROM Temp2;
--- SELECT 36422
+-- SELECT 36430
 -- Query returned successfully in 25 secs 326 msec.
 
 CREATE INDEX Intersections_geom_idx ON Intersections USING GiST(geom);
@@ -99,8 +115,8 @@ WITH Temp (geom) AS (
 )
 SELECT row_number() OVER () AS id, geom
 FROM Temp;
--- SELECT 61878
--- Query returned successfully in 12 min 45 secs.
+-- SELECT 61884
+-- Query returned successfully in 7 min 42 secs.
 
 SELECT DISTINCT geometrytype(geom) FROM Segments;
 -- "LINESTRING"
@@ -111,8 +127,8 @@ SELECT min(ST_NPoints(geom)), max(ST_NPoints(geom)) FROM Segments;
 CREATE INDEX Segments_geom_idx ON Segments USING GIST(geom);
 -- Query returned successfully in 2 secs 648 msec.
 
-DROP TABLE IF EXISTS MyNodes;
-CREATE TABLE MyNodes AS
+DROP TABLE IF EXISTS TempNodes;
+CREATE TABLE TempNodes AS
 WITH Temp(geom) AS (
 	SELECT ST_StartPoint(geom) FROM Segments UNION
 	SELECT ST_EndPoint(geom) FROM Segments
@@ -124,7 +140,7 @@ FROM Temp;
 
 -- Number of vertices obtained by osm2pgrouting
 select count(*) from ways_vertices_pgr;
-66687
+66048
 
 CREATE INDEX MyNodes_geom_idx ON MyNodes USING GIST(geom);
 -- Query returned successfully in 886 msec.
@@ -136,21 +152,65 @@ SELECT S.id, N1.id AS source, N2.id AS target, S.geom,
 FROM Segments S, MyNodes N1, MyNodes N2
 WHERE ST_Intersects(ST_StartPoint(S.geom), N1.geom) AND
 	ST_Intersects(ST_EndPoint(S.geom), N2.geom);
--- SELECT 61878
--- Query returned successfully in 25 secs 357 msec.
+-- SELECT 61884
+-- Query returned successfully in 19 secs 674 msec.
+
+/*
+-- Test whether there are several osm_id associated to the same edge
+
+SELECT E.id, count(distinct R.osm_id)
+FROM MyEdges E, Roads R
+WHERE ST_Intersects(E.geom, R.geom) AND 
+	geometrytype(ST_Intersection(E.geom, R.geom)) = 'LINESTRING'
+GROUP BY E.id
+HAVING count(*) > 1
+
+21288	2
+53005	2
+54548	2
+
+-- Analyze these cases using QGIS 
+
+select E.id, R1.osm_id, R2.osm_id
+from myedges E, roads R1, roads R2
+WHERE R1.osm_id < R2.osm_id AND ST_Intersects(E.geom, R1.geom) AND 
+	geometrytype(ST_Intersection(E.geom, R1.geom)) = 'LINESTRING' AND 
+	ST_Intersects(E.geom, R2.geom) AND 
+	geometrytype(ST_Intersection(E.geom, R2.geom)) = 'LINESTRING'	
+
+21288	24591694	24591696
+53005	490493551	740404157
+54548	490493551	740404156
+
+-- It can be seen that any of the two osm_id associated to the edges
+-- are equivalent
+*/
+
+ALTER TABLE MyEdges ADD COLUMN osm_id bigint;
+UPDATE MyEdges E
+SET osm_id = (
+	SELECT R.osm_id FROM Roads R 
+	WHERE ST_Intersects(E.geom, R.geom) AND 
+		geometrytype(ST_Intersection(E.geom, R.geom)) IN ('LINESTRING', 'MULTILINESTRING')
+	LIMIT 1);
+-- UPDATE 61884
+-- Query returned successfully in 39 secs 275 msec.
+
+SELECT count(*) FROM MyEdges WHERE osm_id IS NULL;
+-- 2109
 
 CREATE UNIQUE INDEX MyEdges_id_idx ON MyEdges USING BTREE(id);
 CREATE INDEX MyEdges_geom_index ON MyEdges USING GiST(geom);
 
 -- Number of ways obtained by osm2pgrouting
 select count(*) from ways;
--- 80831
+-- 79861
 
 -- The nodes table should contain ONLY the vertices that belong to the largest
 -- connected component in the underlying map. Like this, we guarantee that
 -- there will be a non-NULL shortest path between any two nodes.
-DROP TABLE IF EXISTS Nodes;
-CREATE TABLE Nodes AS
+DROP TABLE IF EXISTS MyNodes;
+CREATE TABLE MyNodes AS
 WITH Components AS (
 	SELECT * FROM pgr_strongComponents(
 		'SELECT id, source, target, length_m AS cost, '
