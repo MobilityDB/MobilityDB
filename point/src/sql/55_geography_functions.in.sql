ab37fe373c4f42d6084e1b8c8f377b9b7e68e5c5
Esteban Zimanyi <ezimanyi@ulb.ac.be>
2020-08-26 11:18:30 +0200

    Compatibility with PostGIS version 2.5.5

--- point/src/sql/55_geography_functions.in.sql
+++ point/src/sql/55_geography_functions.in.sql
@@ -13,92 +13,6 @@
  *****************************************************************************/
 
 -- Availability: 3.1.0
-CREATE OR REPLACE FUNCTION ST_GeographyN(geography, integer)
-	RETURNS geography
-	AS 'MODULE_PATHNAME', 'geography_geographyn_collection'
-	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--- Availability: 3.1.0
-CREATE OR REPLACE FUNCTION ST_NumGeographies(geography)
-	RETURNS int4
-	AS 'MODULE_PATHNAME', 'geography_numgeographies_collection'
-	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--------------------------------------------------------------------------
-
--- Availability: 3.1.0
-CREATE OR REPLACE FUNCTION ST_MakeLine(geography[])
-	RETURNS geography
-	AS 'MODULE_PATHNAME', 'geography_makeline_garray'
-	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--- Availability: 3.1.0
-CREATE OR REPLACE FUNCTION ST_LineFromMultiPoint(geography)
-	RETURNS geography
-	AS 'MODULE_PATHNAME', 'geography_line_from_mpoint'
-	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--- Availability: 3.1.0
-CREATE OR REPLACE FUNCTION ST_MakeLine(geography, geography)
-	RETURNS geography
-	AS 'MODULE_PATHNAME', 'geography_makeline'
-	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--------------------------------------------------------------------------
-
--- Availability: 3.1.0
-CREATE OR REPLACE FUNCTION ST_NumPoints(geography)
-	RETURNS int4
-	AS 'MODULE_PATHNAME', 'geography_numpoints_linestring'
-	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--- Availability: 3.1.0
-CREATE OR REPLACE FUNCTION ST_StartPoint(geography)
-	RETURNS geography
-	AS 'MODULE_PATHNAME', 'geography_startpoint_linestring'
-	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--- Availability: 3.1.0
-CREATE OR REPLACE FUNCTION ST_EndPoint(geography)
-	RETURNS geography
-	AS 'MODULE_PATHNAME', 'geography_endpoint_linestring'
-	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--- Availability: 3.1.0
-CREATE OR REPLACE FUNCTION ST_PointN(geography, integer)
-	RETURNS geography
-	AS 'MODULE_PATHNAME','geography_pointn_linestring'
-	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--------------------------------------------------------------------------
-
--- Availability: 3.1.0
-CREATE OR REPLACE FUNCTION ST_AddPoint(geography, geography)
-	RETURNS geography
-	AS 'MODULE_PATHNAME', 'geography_addpoint'
-	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--- Availability: 3.1.0
-CREATE OR REPLACE FUNCTION ST_AddPoint(geography, geography, integer)
-	RETURNS geography
-	AS 'MODULE_PATHNAME', 'geography_addpoint'
-	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--- Availability: 3.1.0
-CREATE OR REPLACE FUNCTION ST_RemovePoint(geography, integer)
-	RETURNS geography
-	AS 'MODULE_PATHNAME', 'geography_removepoint'
-	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--- Availability: 3.1.0
-CREATE OR REPLACE FUNCTION ST_SetPoint(geography, integer, geography)
-	RETURNS geography
-	AS 'MODULE_PATHNAME', 'geography_setpoint_linestring'
-	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--------------------------------------------------------------------------
-
--- Availability: 3.1.0
 CREATE OR REPLACE FUNCTION ST_LineInterpolatePoint(geography, float8, 
 		use_spheroid boolean DEFAULT true)
 	RETURNS geography
@@ -140,14 +54,5 @@ CREATE OR REPLACE FUNCTION ST_ShortestLine(geography, geography,
 	RETURNS geography
 	AS 'MODULE_PATHNAME', 'geography_shortestline'
 	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
--- Availability: 3.1.0
-CREATE OR REPLACE FUNCTION ST_Segmentize1(geog geography, 
-		max_segment_length float8, use_spheroid boolean DEFAULT true)
-	RETURNS geography
-	AS 'MODULE_PATHNAME','geography_segmentize1'
-	LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE
-	COST 100;
-
 
 /*****************************************************************************/
