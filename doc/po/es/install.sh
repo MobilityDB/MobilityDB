b0536ad439f4374ccfba4a8a103a7a173ce7ccf6
Robin <rchoquet@student.42.fr>
2021-03-22 10:32:51 +0100

    add files for spanish documentation

---
+++ doc/po/es/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$#" != "1" ]; then
+	echo "Usage: $0 <install-path>"
+	exit 1
+fi
+
+installpath=$1
+
+cd `dirname $0`
+
+mkdir -p $installpath
+
+xsltproc --stringparam html.stylesheet "docbook.css" --xinclude -o $installpath/index.html /usr/share/xml/docbook/stylesheet/docbook-xsl/html/chunk.xsl mobilitydb-manual.xml
+dblatex -s texstyle.sty -T native -t pdf -o $installpath/mobilitydb-manual.pdf mobilitydb-manual.xml
+dbtoepub -o $installpath/mobilitydb-manual.epub mobilitydb-manual.xml
+
+cp docbook.css $installpath/
+cp -r images $installpath/
+
+## doxygen
+cd ..
+( cat Doxyfile ; echo "OUTPUT_DIRECTORY=$installpath/api" ) | doxygen -
