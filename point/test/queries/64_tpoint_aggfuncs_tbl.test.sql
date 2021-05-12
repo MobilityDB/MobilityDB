ea56fa3d041525964cfcbc88e1f87564fedc5c88
Esteban Zimanyi <ezimanyi@ulb.ac.be>
2021-05-12 14:59:59 +0200

    Improve synthetic data generator

--- point/test/queries/64_tpoint_aggfuncs_tbl.test.sql
+++ point/test/queries/64_tpoint_aggfuncs_tbl.test.sql
@@ -33,83 +33,83 @@ SET force_parallel_mode=regress;
 -- Extent aggregate function
 -------------------------------------------------------------------------------
 
-SELECT setprecision(extent(inst), 13) FROM tbl_tgeompointinst;
-SELECT setprecision(extent(inst), 13) FROM tbl_tgeogpointinst;
-SELECT setprecision(extent(ti), 13) FROM tbl_tgeompointi;
-SELECT setprecision(extent(ti), 13) FROM tbl_tgeogpointi;
-SELECT setprecision(extent(seq), 13) FROM tbl_tgeompointseq;
-SELECT setprecision(extent(seq), 13) FROM tbl_tgeogpointseq;
-SELECT setprecision(extent(ts), 13) FROM tbl_tgeompoints;
-SELECT setprecision(extent(ts), 13) FROM tbl_tgeogpoints;
+SELECT setprecision(extent(inst), 13) FROM tbl_tgeompoint_inst;
+SELECT setprecision(extent(inst), 13) FROM tbl_tgeogpoint_inst;
+SELECT setprecision(extent(ti), 13) FROM tbl_tgeompoint_instset;
+SELECT setprecision(extent(ti), 13) FROM tbl_tgeogpoint_instset;
+SELECT setprecision(extent(seq), 13) FROM tbl_tgeompoint_seq;
+SELECT setprecision(extent(seq), 13) FROM tbl_tgeogpoint_seq;
+SELECT setprecision(extent(ts), 13) FROM tbl_tgeompoint_seqset;
+SELECT setprecision(extent(ts), 13) FROM tbl_tgeogpoint_seqset;
 SELECT setprecision(extent(temp), 13) FROM tbl_tgeompoint;
 SELECT setprecision(extent(temp), 13) FROM tbl_tgeogpoint;
 
-SELECT setprecision(extent(inst), 13) FROM tbl_tgeompoint3Dinst;
-SELECT setprecision(extent(inst), 13) FROM tbl_tgeogpoint3Dinst;
-SELECT setprecision(extent(ti), 13) FROM tbl_tgeompoint3Di;
-SELECT setprecision(extent(ti), 13) FROM tbl_tgeogpoint3Di;
-SELECT setprecision(extent(seq), 13) FROM tbl_tgeompoint3Dseq;
-SELECT setprecision(extent(seq), 13) FROM tbl_tgeogpoint3Dseq;
-SELECT setprecision(extent(ts), 13) FROM tbl_tgeompoint3Ds;
-SELECT setprecision(extent(ts), 13) FROM tbl_tgeogpoint3Ds;
+SELECT setprecision(extent(inst), 13) FROM tbl_tgeompoint3D_inst;
+SELECT setprecision(extent(inst), 13) FROM tbl_tgeogpoint3D_inst;
+SELECT setprecision(extent(ti), 13) FROM tbl_tgeompoint3D_instset;
+SELECT setprecision(extent(ti), 13) FROM tbl_tgeogpoint3D_instset;
+SELECT setprecision(extent(seq), 13) FROM tbl_tgeompoint3D_seq;
+SELECT setprecision(extent(seq), 13) FROM tbl_tgeogpoint3D_seq;
+SELECT setprecision(extent(ts), 13) FROM tbl_tgeompoint3D_seqset;
+SELECT setprecision(extent(ts), 13) FROM tbl_tgeogpoint3D_seqset;
 SELECT setprecision(extent(temp), 13) FROM tbl_tgeompoint3D;
 SELECT setprecision(extent(temp), 13) FROM tbl_tgeogpoint3D;
 
 -------------------------------------------------------------------------------
 
-SELECT numInstants(tcentroid(inst)) FROM tbl_tgeompointinst;
-SELECT numInstants(tcount(inst)) FROM tbl_tgeompointinst;
-SELECT k%10, numInstants(tcentroid(inst)) FROM tbl_tgeompointinst GROUP BY k%10 ORDER BY k%10;
-SELECT k%10, numInstants(tcount(inst)) FROM tbl_tgeompointinst GROUP BY k%10 ORDER BY k%10;
+SELECT numInstants(tcentroid(inst)) FROM tbl_tgeompoint_inst;
+SELECT numInstants(tcount(inst)) FROM tbl_tgeompoint_inst;
+SELECT k%10, numInstants(tcentroid(inst)) FROM tbl_tgeompoint_inst GROUP BY k%10 ORDER BY k%10;
+SELECT k%10, numInstants(tcount(inst)) FROM tbl_tgeompoint_inst GROUP BY k%10 ORDER BY k%10;
 
-SELECT numInstants(tcentroid(ti)) FROM tbl_tgeompointi;
-SELECT numInstants(tcount(ti)) FROM tbl_tgeompointi;
-SELECT k%10, numInstants(tcentroid(ti)) FROM tbl_tgeompointi GROUP BY k%10 ORDER BY k%10;
-SELECT k%10, numInstants(tcount(ti)) FROM tbl_tgeompointi GROUP BY k%10 ORDER BY k%10;
+SELECT numInstants(tcentroid(ti)) FROM tbl_tgeompoint_instset;
+SELECT numInstants(tcount(ti)) FROM tbl_tgeompoint_instset;
+SELECT k%10, numInstants(tcentroid(ti)) FROM tbl_tgeompoint_instset GROUP BY k%10 ORDER BY k%10;
+SELECT k%10, numInstants(tcount(ti)) FROM tbl_tgeompoint_instset GROUP BY k%10 ORDER BY k%10;
 
-SELECT numSequences(tcentroid(seq)) FROM tbl_tgeompointseq;
-SELECT numSequences(tcount(seq)) FROM tbl_tgeompointseq;
-SELECT k%10, numSequences(tcentroid(seq)) FROM tbl_tgeompointseq GROUP BY k%10 ORDER BY k%10;
-SELECT k%10, numSequences(tcount(seq)) FROM tbl_tgeompointseq GROUP BY k%10 ORDER BY k%10;
+SELECT numSequences(tcentroid(seq)) FROM tbl_tgeompoint_seq;
+SELECT numSequences(tcount(seq)) FROM tbl_tgeompoint_seq;
+SELECT k%10, numSequences(tcentroid(seq)) FROM tbl_tgeompoint_seq GROUP BY k%10 ORDER BY k%10;
+SELECT k%10, numSequences(tcount(seq)) FROM tbl_tgeompoint_seq GROUP BY k%10 ORDER BY k%10;
 
-SELECT numSequences(tcentroid(ts)) FROM tbl_tgeompoints;
-SELECT numSequences(tcount(ts)) FROM tbl_tgeompoints;
-SELECT k%10, numSequences(tcentroid(ts)) FROM tbl_tgeompoints GROUP BY k%10 ORDER BY k%10;
-SELECT k%10, numSequences(tcount(ts)) FROM tbl_tgeompoints GROUP BY k%10 ORDER BY k%10;
+SELECT numSequences(tcentroid(ts)) FROM tbl_tgeompoint_seqset;
+SELECT numSequences(tcount(ts)) FROM tbl_tgeompoint_seqset;
+SELECT k%10, numSequences(tcentroid(ts)) FROM tbl_tgeompoint_seqset GROUP BY k%10 ORDER BY k%10;
+SELECT k%10, numSequences(tcount(ts)) FROM tbl_tgeompoint_seqset GROUP BY k%10 ORDER BY k%10;
 
-SELECT numInstants(tcentroid(inst)) FROM tbl_tgeompoint3Dinst;
-SELECT numInstants(tcount(inst)) FROM tbl_tgeompoint3Dinst;
-SELECT k%10, numInstants(tcentroid(inst)) FROM tbl_tgeompoint3Dinst GROUP BY k%10 ORDER BY k%10;
-SELECT k%10, numInstants(tcount(inst)) FROM tbl_tgeompoint3Dinst GROUP BY k%10 ORDER BY k%10;
+SELECT numInstants(tcentroid(inst)) FROM tbl_tgeompoint3D_inst;
+SELECT numInstants(tcount(inst)) FROM tbl_tgeompoint3D_inst;
+SELECT k%10, numInstants(tcentroid(inst)) FROM tbl_tgeompoint3D_inst GROUP BY k%10 ORDER BY k%10;
+SELECT k%10, numInstants(tcount(inst)) FROM tbl_tgeompoint3D_inst GROUP BY k%10 ORDER BY k%10;
 
-SELECT numInstants(tcentroid(ti)) FROM tbl_tgeompoint3Di;
-SELECT numInstants(tcount(ti)) FROM tbl_tgeompoint3Di;
-SELECT k%10, numInstants(tcentroid(ti)) FROM tbl_tgeompoint3Di GROUP BY k%10 ORDER BY k%10;
-SELECT k%10, numInstants(tcount(ti)) FROM tbl_tgeompoint3Di GROUP BY k%10 ORDER BY k%10;
+SELECT numInstants(tcentroid(ti)) FROM tbl_tgeompoint3D_instset;
+SELECT numInstants(tcount(ti)) FROM tbl_tgeompoint3D_instset;
+SELECT k%10, numInstants(tcentroid(ti)) FROM tbl_tgeompoint3D_instset GROUP BY k%10 ORDER BY k%10;
+SELECT k%10, numInstants(tcount(ti)) FROM tbl_tgeompoint3D_instset GROUP BY k%10 ORDER BY k%10;
 
-SELECT numSequences(tcentroid(seq)) FROM tbl_tgeompoint3Dseq;
-SELECT numSequences(tcount(seq)) FROM tbl_tgeompoint3Dseq;
-SELECT k%10, numSequences(tcentroid(seq)) FROM tbl_tgeompoint3Dseq GROUP BY k%10 ORDER BY k%10;
-SELECT k%10, numSequences(tcount(seq)) FROM tbl_tgeompoint3Dseq GROUP BY k%10 ORDER BY k%10;
+SELECT numSequences(tcentroid(seq)) FROM tbl_tgeompoint3D_seq;
+SELECT numSequences(tcount(seq)) FROM tbl_tgeompoint3D_seq;
+SELECT k%10, numSequences(tcentroid(seq)) FROM tbl_tgeompoint3D_seq GROUP BY k%10 ORDER BY k%10;
+SELECT k%10, numSequences(tcount(seq)) FROM tbl_tgeompoint3D_seq GROUP BY k%10 ORDER BY k%10;
 
-SELECT numSequences(tcentroid(ts)) FROM tbl_tgeompoint3Ds;
-SELECT numSequences(tcount(ts)) FROM tbl_tgeompoint3Ds;
-SELECT k%10, numSequences(tcentroid(ts)) FROM tbl_tgeompoint3Ds GROUP BY k%10 ORDER BY k%10;
-SELECT k%10, numSequences(tcount(ts)) FROM tbl_tgeompoint3Ds GROUP BY k%10 ORDER BY k%10;
+SELECT numSequences(tcentroid(ts)) FROM tbl_tgeompoint3D_seqset;
+SELECT numSequences(tcount(ts)) FROM tbl_tgeompoint3D_seqset;
+SELECT k%10, numSequences(tcentroid(ts)) FROM tbl_tgeompoint3D_seqset GROUP BY k%10 ORDER BY k%10;
+SELECT k%10, numSequences(tcount(ts)) FROM tbl_tgeompoint3D_seqset GROUP BY k%10 ORDER BY k%10;
 
 -------------------------------------------------------------------------------
 
-SELECT numInstants(tcount(inst)) FROM tbl_tgeogpointinst;
-SELECT k%10, numInstants(tcount(inst)) FROM tbl_tgeogpointinst GROUP BY k%10 ORDER BY k%10;
+SELECT numInstants(tcount(inst)) FROM tbl_tgeogpoint_inst;
+SELECT k%10, numInstants(tcount(inst)) FROM tbl_tgeogpoint_inst GROUP BY k%10 ORDER BY k%10;
 
-SELECT numInstants(tcount(ti)) FROM tbl_tgeogpointi;
-SELECT k%10, numInstants(tcount(ti)) FROM tbl_tgeogpointi GROUP BY k%10 ORDER BY k%10;
+SELECT numInstants(tcount(ti)) FROM tbl_tgeogpoint_instset;
+SELECT k%10, numInstants(tcount(ti)) FROM tbl_tgeogpoint_instset GROUP BY k%10 ORDER BY k%10;
 
-SELECT numSequences(tcount(seq)) FROM tbl_tgeogpointseq;
-SELECT k%10, numSequences(tcount(seq)) FROM tbl_tgeogpointseq GROUP BY k%10 ORDER BY k%10;
+SELECT numSequences(tcount(seq)) FROM tbl_tgeogpoint_seq;
+SELECT k%10, numSequences(tcount(seq)) FROM tbl_tgeogpoint_seq GROUP BY k%10 ORDER BY k%10;
 
-SELECT numSequences(tcount(ts)) FROM tbl_tgeogpoints;
-SELECT k%10, numSequences(tcount(ts)) FROM tbl_tgeogpoints GROUP BY k%10 ORDER BY k%10;
+SELECT numSequences(tcount(ts)) FROM tbl_tgeogpoint_seqset;
+SELECT k%10, numSequences(tcount(ts)) FROM tbl_tgeogpoint_seqset GROUP BY k%10 ORDER BY k%10;
 
 -------------------------------------------------------------------------------
 
--- point/test/queries/68_tpoint_tempspatialrels_tbl.test.sql
+++ point/test/queries/68_tpoint_tempspatialrels_tbl.test.sql
@@ -30,36 +30,36 @@
 -- tcontains
 -------------------------------------------------------------------------------
 
-SELECT count(*) FROM tbl_geompoint, tbl_tgeompoint
+SELECT count(*) FROM tbl_geom_point, tbl_tgeompoint
   WHERE tcontains(g, temp) IS NOT NULL;
-SELECT count(*) FROM tbl_tgeompoint, tbl_geompoint
+SELECT count(*) FROM tbl_tgeompoint, tbl_geom_point
   WHERE tcontains(temp, g) IS NOT NULL;
 
 -------------------------------------------------------------------------------
 -- tcovers
 -------------------------------------------------------------------------------
 
-SELECT count(*) FROM tbl_geompoint, tbl_tgeompoint
+SELECT count(*) FROM tbl_geom_point, tbl_tgeompoint
   WHERE tcovers(g, temp) IS NOT NULL;
-SELECT count(*) FROM tbl_tgeompoint, tbl_geompoint
+SELECT count(*) FROM tbl_tgeompoint, tbl_geom_point
   WHERE tcovers(temp, g) IS NOT NULL;
 
 -------------------------------------------------------------------------------
 -- tcoveredby
 -------------------------------------------------------------------------------
 
-SELECT count(*) FROM tbl_geompoint, tbl_tgeompoint
+SELECT count(*) FROM tbl_geom_point, tbl_tgeompoint
   WHERE tcoveredby(g, temp) IS NOT NULL;
-SELECT count(*) FROM tbl_tgeompoint, tbl_geompoint
+SELECT count(*) FROM tbl_tgeompoint, tbl_geom_point
   WHERE tcoveredby(temp, g) IS NOT NULL;
 
 -------------------------------------------------------------------------------
 -- tdisjoint
 -------------------------------------------------------------------------------
 
-SELECT count(*) FROM tbl_geompoint, tbl_tgeompoint
+SELECT count(*) FROM tbl_geom_point, tbl_tgeompoint
   WHERE tdisjoint(g, temp) IS NOT NULL;
-SELECT count(*) FROM tbl_tgeompoint, tbl_geompoint
+SELECT count(*) FROM tbl_tgeompoint, tbl_geom_point
   WHERE tdisjoint(temp, g) IS NOT NULL;
 SELECT count(*) FROM tbl_tgeompoint t1, tbl_tgeompoint t2
   WHERE tdisjoint(t1.temp, t2.temp) IS NOT NULL;
@@ -71,9 +71,9 @@ SELECT count(*) FROM tbl_tgeogpoint t1, tbl_tgeogpoint t2
 -- tequals
 -------------------------------------------------------------------------------
 
-SELECT count(*) FROM tbl_geompoint, tbl_tgeompoint
+SELECT count(*) FROM tbl_geom_point, tbl_tgeompoint
   WHERE tequals(g, temp) IS NOT NULL;
-SELECT count(*) FROM tbl_tgeompoint, tbl_geompoint
+SELECT count(*) FROM tbl_tgeompoint, tbl_geom_point
   WHERE tequals(temp, g) IS NOT NULL;
 SELECT count(*) FROM tbl_tgeompoint t1, tbl_tgeompoint t2
   WHERE tequals(t1.temp, t2.temp) IS NOT NULL;
@@ -82,9 +82,9 @@ SELECT count(*) FROM tbl_tgeompoint t1, tbl_tgeompoint t2
 -- tintersects
 -------------------------------------------------------------------------------
 
-SELECT count(*) FROM tbl_geompoint, tbl_tgeompoint
+SELECT count(*) FROM tbl_geom_point, tbl_tgeompoint
   WHERE tintersects(g, temp) IS NOT NULL;
-SELECT count(*) FROM tbl_tgeompoint, tbl_geompoint
+SELECT count(*) FROM tbl_tgeompoint, tbl_geom_point
   WHERE tintersects(temp, g) IS NOT NULL;
 SELECT count(*) FROM tbl_tgeompoint t1, tbl_tgeompoint t2
   WHERE tintersects(t1.temp, t2.temp) IS NOT NULL;
@@ -96,27 +96,27 @@ SELECT count(*) FROM tbl_tgeogpoint t1, tbl_tgeogpoint t2
 -- ttouches
 -------------------------------------------------------------------------------
 
-SELECT count(*) FROM tbl_geompoint, tbl_tgeompoint
+SELECT count(*) FROM tbl_geom_point, tbl_tgeompoint
   WHERE ttouches(g, temp) IS NOT NULL;
-SELECT count(*) FROM tbl_tgeompoint, tbl_geompoint
+SELECT count(*) FROM tbl_tgeompoint, tbl_geom_point
   WHERE ttouches(temp, g) IS NOT NULL;
 
 -------------------------------------------------------------------------------
 -- twithin
 -------------------------------------------------------------------------------
 
-SELECT count(*) FROM tbl_geompoint, tbl_tgeompoint
+SELECT count(*) FROM tbl_geom_point, tbl_tgeompoint
   WHERE twithin(g, temp) IS NOT NULL;
-SELECT count(*) FROM tbl_tgeompoint, tbl_geompoint
+SELECT count(*) FROM tbl_tgeompoint, tbl_geom_point
   WHERE twithin(temp, g) IS NOT NULL;
 
 -------------------------------------------------------------------------------
 -- tdwithin
 -------------------------------------------------------------------------------
 
-SELECT count(*) FROM tbl_geompoint, tbl_tgeompoint
+SELECT count(*) FROM tbl_geom_point, tbl_tgeompoint
   WHERE tdwithin(g, temp, 10) IS NOT NULL;
-SELECT count(*) FROM tbl_tgeompoint, tbl_geompoint
+SELECT count(*) FROM tbl_tgeompoint, tbl_geom_point
   WHERE tdwithin(temp, g, 10) IS NOT NULL;
 SELECT count(*) FROM tbl_tgeompoint t1, tbl_tgeompoint t2
   WHERE tdwithin(t1.temp, t2.temp, 10) IS NOT NULL;
@@ -128,9 +128,9 @@ SELECT count(*) FROM tbl_tgeogpoint t1, tbl_tgeogpoint t2
 -- trelate (2 arguments returns text)
 -------------------------------------------------------------------------------
 
-SELECT count(*) FROM tbl_geompoint, tbl_tgeompoint
+SELECT count(*) FROM tbl_geom_point, tbl_tgeompoint
   WHERE trelate(g, temp) IS NOT NULL;
-SELECT count(*) FROM tbl_tgeompoint, tbl_geompoint
+SELECT count(*) FROM tbl_tgeompoint, tbl_geom_point
   WHERE trelate(temp, g) IS NOT NULL;
 SELECT count(*) FROM tbl_tgeompoint t1, tbl_tgeompoint t2
   WHERE trelate(t1.temp, t2.temp) IS NOT NULL;
@@ -139,9 +139,9 @@ SELECT count(*) FROM tbl_tgeompoint t1, tbl_tgeompoint t2
 -- trelate (3 arguments returns boolean)
 -------------------------------------------------------------------------------
 
-SELECT count(*) FROM tbl_geompoint, tbl_tgeompoint
+SELECT count(*) FROM tbl_geom_point, tbl_tgeompoint
   WHERE trelate(g, temp, 'T*****FF*') IS NOT NULL;
-SELECT count(*) FROM tbl_tgeompoint, tbl_geompoint
+SELECT count(*) FROM tbl_tgeompoint, tbl_geom_point
   WHERE trelate(temp, g, 'T*****FF*') IS NOT NULL;
 SELECT count(*) FROM tbl_tgeompoint t1, tbl_tgeompoint t2
   WHERE trelate(t1.temp, t2.temp, 'T*****FF*') IS NOT NULL;
