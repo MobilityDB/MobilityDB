73f5be5481ffda854a706433ac5b491f7bf0f6f9
Esteban Zimanyi <ezimanyi@ulb.ac.be>
2021-05-18 17:15:44 +0200

    Increase coverage

--- test/queries/07_rangetype_ext_tbl.test.sql
+++ test/queries/07_rangetype_ext_tbl.test.sql
@@ -69,7 +69,7 @@ set parallel_tuple_cost=0;
 set min_parallel_table_scan_size=0;
 set max_parallel_workers_per_gather=2;
 
-SELECT extent(temp::floatrange) FROM tbl_tfloat_big;
+SELECT setPrecision(extent(temp::floatrange),6) FROM tbl_tfloat_big;
 
 -- reset to default values 
 reset parallel_setup_cost;
