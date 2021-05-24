4d829a136ed059ce5f42f975521ad8090f2855e1
Esteban Zimanyi <ezimanyi@ulb.ac.be>
2021-05-25 00:33:55 +0200

    Revert back to previous constructors

--- point/test/queries/54_tpoint_compops.test.sql
+++ point/test/queries/54_tpoint_compops.test.sql
@@ -6,20 +6,20 @@
 -- contributors
 --
 -- Permission to use, copy, modify, and distribute this software and its
--- documentation for any purpose, without fee, and without a written 
+-- documentation for any purpose, without fee, and without a written
 -- agreement is hereby granted, provided that the above copyright notice and
 -- this paragraph and the following two paragraphs appear in all copies.
 --
 -- IN NO EVENT SHALL UNIVERSITE LIBRE DE BRUXELLES BE LIABLE TO ANY PARTY FOR
 -- DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING
 -- LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION,
--- EVEN IF UNIVERSITE LIBRE DE BRUXELLES HAS BEEN ADVISED OF THE POSSIBILITY 
+-- EVEN IF UNIVERSITE LIBRE DE BRUXELLES HAS BEEN ADVISED OF THE POSSIBILITY
 -- OF SUCH DAMAGE.
 --
--- UNIVERSITE LIBRE DE BRUXELLES SPECIFICALLY DISCLAIMS ANY WARRANTIES, 
+-- UNIVERSITE LIBRE DE BRUXELLES SPECIFICALLY DISCLAIMS ANY WARRANTIES,
 -- INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
 -- AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON
--- AN "AS IS" BASIS, AND UNIVERSITE LIBRE DE BRUXELLES HAS NO OBLIGATIONS TO 
+-- AN "AS IS" BASIS, AND UNIVERSITE LIBRE DE BRUXELLES HAS NO OBLIGATIONS TO
 -- PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS. 
 --
 -------------------------------------------------------------------------------
@@ -100,7 +100,7 @@ SELECT tgeompoint '[Point(1 1 1)@2000-01-01, Point(2 1 2)@2000-01-02]' #= tgeomp
 SELECT tgeompoint '[Point(1 1 1)@2000-01-01, Point(2 2 1)@2000-01-02]' #= tgeompoint '[Point(2 1 1)@2000-01-01, Point(1 2 1)@2000-01-02]';
 SELECT tgeompoint '[Point(1 1 1)@2000-01-01, Point(2 3 1)@2000-01-02]' #= tgeompoint '[Point(2 2 1)@2000-01-01, Point(1 2 1)@2000-01-02]';
 SELECT tgeompoint '[Point(1 1 1)@2000-01-01, Point(2 2 1)@2000-01-02]' #= tgeompoint '[Point(2 2 0.5)@2000-01-01, Point(3 3 1.5)@2000-01-02]';
- 
+
 SELECT tgeompoint 'Point(1 1 1)@2000-01-01' #= tgeompoint 'Point(1 1 1)@2000-01-01';
 SELECT tgeompoint '{Point(1 1 1)@2000-01-01, Point(2 2 2)@2000-01-02, Point(1 1 1)@2000-01-03}' #= tgeompoint 'Point(1 1 1)@2000-01-01';
 SELECT tgeompoint '[Point(1 1 1)@2000-01-01, Point(2 2 2)@2000-01-02, Point(1 1 1)@2000-01-03]' #= tgeompoint 'Point(1 1 1)@2000-01-01';
@@ -124,8 +124,8 @@ SELECT tgeompoint 'Interp=Stepwise;[Point(1 1 1)@2000-01-01, Point(2 2 2)@2000-0
 SELECT tgeompoint 'Interp=Stepwise;{[Point(1 1 1)@2000-01-01, Point(2 2 2)@2000-01-02, Point(1 1 1)@2000-01-03],[Point(3 3 3)@2000-01-04, Point(3 3 3)@2000-01-05]}' #= tgeompoint 'Interp=Stepwise;{[Point(1 1 1)@2000-01-01, Point(2 2 2)@2000-01-02, Point(1 1 1)@2000-01-03],[Point(3 3 3)@2000-01-04, Point(3 3 3)@2000-01-05]}';
 
 /* Roundoff errors  */
-SELECT tgeompoint '[Point(1 1)@2000-01-01, Point(2 2)@2000-01-02]' #=  tgeompoint(ST_MakePoint(1+1e-12, 1+1e-12), period '[2000-01-01, 2000-01-02]');
-SELECT tgeompoint '[Point(1 1)@2000-01-01, Point(2 2)@2000-01-02]' #=  tgeompoint(ST_MakePoint(1+1e-13, 1+1e-13), period '[2000-01-01, 2000-01-02]');
+SELECT tgeompoint '[Point(1 1)@2000-01-01, Point(2 2)@2000-01-02]' #=  tgeompoint_seq(ST_MakePoint(1+1e-12, 1+1e-12), period '[2000-01-01, 2000-01-02]');
+SELECT tgeompoint '[Point(1 1)@2000-01-01, Point(2 2)@2000-01-02]' #=  tgeompoint_seq(ST_MakePoint(1+1e-13, 1+1e-13), period '[2000-01-01, 2000-01-02]');
 
 -------------------------------------------------------------------------------
 
