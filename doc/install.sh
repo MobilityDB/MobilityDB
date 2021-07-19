231a00b0471f3f734e2c1bee32f92752133c91a8
Esteban Zimanyi <ezimanyi@ulb.ac.be>
2021-07-19 20:17:44 +0200

    Check licences and shell scripts

--- doc/install.sh
+++ doc/install.sh
@@ -7,33 +7,32 @@ fi
 
 installpath=$1
 
-cd `dirname $0`
+cd $(dirname "$0") || exit
 
-mkdir -p $installpath
+mkdir -p "$installpath"
 
-xsltproc --stringparam html.stylesheet "docbook.css" --xinclude -o $installpath/index.html /usr/share/xml/docbook/stylesheet/docbook-xsl-ns/html/chunk.xsl mobilitydb-manual.xml
-dblatex -s texstyle.sty -T native -t pdf -o $installpath/mobilitydb-manual.pdf mobilitydb-manual.xml
-dbtoepub -o $installpath/mobilitydb-manual.epub mobilitydb-manual.xml
+xsltproc --stringparam html.stylesheet "docbook.css" --xinclude -o "$installpath"/index.html /usr/share/xml/docbook/stylesheet/docbook-xsl-ns/html/chunk.xsl mobilitydb-manual.xml
+dblatex -s texstyle.sty -T native -t pdf -o "$installpath"/mobilitydb-manual.pdf mobilitydb-manual.xml
+dbtoepub -o "$installpath"/mobilitydb-manual.epub mobilitydb-manual.xml
 
-cp docbook.css $installpath/
-cp -r images $installpath/
+cp docbook.css "$installpath"/
+cp -r images "$installpath"/
 
 ## doxygen
 cd ..
 ( cat Doxyfile ; echo "OUTPUT_DIRECTORY=$installpath/api" ) | doxygen -
 
-cd doc
+cd doc || exit
 
-############# Spanish Traduction ##############
-
+############# Spanish Translation ##############
 
 installpathES="${installpath}/es"
 
-mkdir -p $installpathES
+mkdir -p "$installpathES"
 
-xsltproc --stringparam html.stylesheet "docbook.css" --xinclude -o $installpathES/index.html /usr/share/xml/docbook/stylesheet/docbook-xsl-ns/html/chunk.xsl po/es/mobilitydb-manual.xml
-dblatex -s texstyle.sty -T native -t pdf -o $installpathES/mobilitydb-manual.pdf po/es/mobilitydb-manual.xml
-dbtoepub -o $installpathES/mobilitydb-manual.epub po/es/mobilitydb-manual.xml
+xsltproc --stringparam html.stylesheet "docbook.css" --xinclude -o "$installpathES"/index.html /usr/share/xml/docbook/stylesheet/docbook-xsl-ns/html/chunk.xsl po/es/mobilitydb-manual.xml
+dblatex -s texstyle.sty -T native -t pdf -o "$installpathES"/mobilitydb-manual.pdf po/es/mobilitydb-manual.xml
+dbtoepub -o "$installpathES"/mobilitydb-manual.epub po/es/mobilitydb-manual.xml
 
-cp docbook.css $installpathES/
-cp -r images $installpathES/
+cp docbook.css "$installpathES"/
+cp -r images "$installpathES"/
--- npoint/src/datagen/create_test_tables_tnpoint.sql
+++ npoint/src/datagen/create_test_tables_tnpoint.sql
@@ -1,3 +1,38 @@
+/*****************************************************************************
+ *
+ * This MobilityDB code is provided under The PostgreSQL License.
+ *
+ * Copyright (c) 2016-2021, Université libre de Bruxelles and MobilityDB
+ * contributors
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation FOR any purpose, without fee, and without a written
+ * agreement is hereby granted, provided that the above copyright notice and
+ * this paragraph and the following two paragraphs appear in all copies.
+ *
+ * IN NO EVENT SHALL UNIVERSITE LIBRE DE BRUXELLES BE LIABLE TO ANY PARTY FOR
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING
+ * LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION,
+ * EVEN IF UNIVERSITE LIBRE DE BRUXELLES HAS BEEN ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ *
+ * UNIVERSITE LIBRE DE BRUXELLES SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON
+ * AN "AS IS" BASIS, AND UNIVERSITE LIBRE DE BRUXELLES HAS NO OBLIGATIONS TO
+ * PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS. 
+ *
+ *****************************************************************************/
+
+/*
+ * create_test_tables_tnpoint.sql
+ * Function generating a set of test tables for temporal network point types.
+ *
+ * These functions use the random generator for these types that are in the
+ * file random_tnpoint.sql. Refer to that file for the meaning of the
+ * parameters used in the function calls of this file.
+ */
+
 DROP FUNCTION IF EXISTS create_test_tables_npoint();
 CREATE OR REPLACE FUNCTION create_test_tables_npoint(size int DEFAULT 1000)
 RETURNS text AS $$
