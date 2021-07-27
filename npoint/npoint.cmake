1e117f97edada906e43e833575a38daa4037b236
cvvergara <vicky@georepublic.de>
2021-07-26 21:37:21 -0500

    [build] removing unused code

--- npoint/npoint.cmake
+++ npoint/npoint.cmake
@@ -1,21 +1,2 @@
 add_definitions(-DWITH_POSTGIS)
-#[[
-set(SRCNPOINT
-npoint/src/tnpoint.c
-npoint/src/tnpoint_aggfuncs.c
-npoint/src/tnpoint_analyze.c
-npoint/src/tnpoint_boxops.c
-npoint/src/tnpoint_distance.c
-npoint/src/tnpoint_indexes.c
-npoint/src/tnpoint_parser.c
-npoint/src/tnpoint_posops.c
-npoint/src/tnpoint_selfuncs.c
-npoint/src/tnpoint_spatialfuncs.c
-npoint/src/tnpoint_spatialrels.c
-npoint/src/tnpoint_static.c
-npoint/src/tnpoint_tempspatialrels.c
-)
-
-target_sources(${CMAKE_PROJECT_NAME} PRIVATE ${SRCNPOINT})
-]]
 set(CONTROLIN "${CONTROLIN};npoint/control.in")
