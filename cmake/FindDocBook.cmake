b921ab05dd2f364aa6399b1dcc578f8fd4215cad
Esteban Zimanyi <ezimanyi@ulb.ac.be>
2021-08-15 16:16:55 +0200

    Add target for building the documentation. Fix Issue #117

--- cmake/FindDocBook.cmake
+++ cmake/FindDocBook.cmake
@@ -8,9 +8,9 @@ find_path(DOCBOOK_DTD
   docbookx.dtd
   PATHS ${CMAKE_SYSTEM_PREFIX_PATH} ${DOCBOOK_ROOT} ENV DOCBOOK_ROOT
   PATH_SUFFIXES
-    share/xml/docbook/schema/dtd/4.2
+    share/xml/docbook/schema/dtd/4.5
     docbook-dtd
-    share/sgml/docbook/xml-dtd-4.2)
+    share/sgml/docbook/xml-dtd-4.5)
 
 find_path(DOCBOOK_XSL
   NAMES html/html.xsl xhtml-1_1/html.xsl
