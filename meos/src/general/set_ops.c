137eb9735d12fb21e1dc9734201f30f42b838527
Maxime Schoemans <maxime.schoemans@ulb.ac.be>
2023-01-07 19:38:49 -0500

    Add MEOS function union_timestamp_timestampset

--- meos/src/general/set_ops.c
+++ meos/src/general/set_ops.c
@@ -1166,7 +1166,18 @@ union_textset_text(const Set *s, text *txt)
 Set *
 union_tstzset_timestamp(const Set *ts, const TimestampTz t)
 {
-  return union_timestamp_tstzset(t, ts);
+  return union_set_value(ts, TimestampTzGetDatum(t), T_TIMESTAMPTZ);
+}
+
+/**
+ * @ingroup libmeos_setspan_set
+ * @brief Return the union of a timestamp and a timestamp set
+ * @sqlop @p +
+ */
+TimestampSet *
+union_timestamp_tstzset(const TimestampTz t, const TimestampSet *ts)
+{
+  return union_tstzset_timestamp(ts, t);
 }
 #endif /* MEOS */
 
