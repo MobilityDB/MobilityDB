802b3c87901f7f8fed01866e38af28b514b37789
Esteban Zimanyi <ezimanyi@ulb.ac.be>
2023-05-16 15:17:02 +0200

    Increase coverage

--- meos/src/general/temporal.c
+++ meos/src/general/temporal.c
@@ -3889,7 +3889,11 @@ temporal_stops(const Temporal *temp, double maxdist,
 {
   if (maxdist < 0)
     elog(ERROR, "The maximum distance must be positive: %f", maxdist);
-  ensure_valid_duration(minduration);
+  Interval intervalzero;
+  memset(&intervalzero, 0, sizeof(Interval));
+  int cmp = pg_interval_cmp(minduration, &intervalzero);
+  if (cmp < 0)
+    elog(ERROR, "The duration must be positive");
   int64 mintunits = interval_units(minduration);
 
   TSequenceSet *result = NULL;
