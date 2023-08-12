b7b51185882a5b0f94320e563e1bffed144e506b
Esteban Zimanyi <estebanzimanyi@gmail.com>
2023-08-12 22:50:32 +0200

    Fix kdtree index

--- mobilitydb/src/general/span_spgist.c
+++ mobilitydb/src/general/span_spgist.c
@@ -266,10 +266,10 @@ get_quadrant2D(const Span *centroid, const Span *query)
 }
 
 /**
- * @brief Can any span from nodebox overlap with this argument?
+ * @brief Can any span from nodebox overlap with the query?
  */
 static bool
-overlap2D(const SpanNode *nodebox, const Span *query)
+overlap2D_quad(const SpanNode *nodebox, const Span *query)
 {
   Span s;
   span_set(nodebox->left.lower, nodebox->right.upper, nodebox->left.lower_inc,
@@ -278,14 +278,54 @@ overlap2D(const SpanNode *nodebox, const Span *query)
 }
 
 /**
+ * @brief Can any span from nodebox overlap with the query?
+ */
+static bool
+overlap2D_kd(const SpanNode *nodebox, const Span *query, int level)
+{
+  Span s;
+  if (level % 2)
+  {
+    span_set(nodebox->left.lower, nodebox->right.lower, nodebox->left.lower_inc,
+      nodebox->right.lower_inc, nodebox->left.basetype, &s);    
+  }
+  else
+  {
+    span_set(nodebox->left.upper, nodebox->right.upper, nodebox->left.upper_inc,
+      nodebox->right.upper_inc, nodebox->left.basetype, &s);
+  }
+  return overlaps_span_span(&s, query);
+}
+
+/**
  * @brief Can any span from nodebox contain the query?
  */
 static bool
-contain2D(const SpanNode *nodebox, const Span *query)
+contain2D_quad(const SpanNode *nodebox, const Span *query)
 {
   Span s;
   span_set(nodebox->left.lower, nodebox->right.upper, nodebox->left.lower_inc,
     nodebox->right.upper_inc, nodebox->left.basetype, &s);
+  return contains_span_span(&s, query);
+}
+
+/**
+ * @brief Can any span from nodebox contain the query?
+ */
+static bool
+contain2D_kd(const SpanNode *nodebox, const Span *query, int level)
+{
+  Span s;
+  if (level % 2)
+  {
+    span_set(nodebox->left.lower, nodebox->right.lower, nodebox->left.lower_inc,
+      nodebox->right.lower_inc, nodebox->left.basetype, &s);    
+  }
+  else
+  {
+    span_set(nodebox->left.upper, nodebox->right.upper, nodebox->left.upper_inc,
+      nodebox->right.upper_inc, nodebox->left.basetype, &s);
+  }
   return contains_span_span(&s, query);
 }
 
@@ -857,12 +897,16 @@ Span_spgist_inner_consistent(FunctionCallInfo fcinfo, SPGistIndexType idxtype)
         case RTOverlapStrategyNumber:
         case RTContainedByStrategyNumber:
         case RTAdjacentStrategyNumber:
-          flag = overlap2D(&next_nodespan, &queries[i]);
+          flag = (idxtype == SPGIST_QUADTREE) ?
+            overlap2D_quad(&next_nodespan, &queries[i]) :
+            overlap2D_kd(&next_nodespan, &queries[i], in->level);
           break;
         case RTContainsStrategyNumber:
         case RTEqualStrategyNumber:
         case RTSameStrategyNumber:
-          flag = contain2D(&next_nodespan, &queries[i]);
+          flag = (idxtype == SPGIST_QUADTREE) ?
+            contain2D_quad(&next_nodespan, &queries[i]) :
+            contain2D_kd(&next_nodespan, &queries[i], in->level);
           break;
         case RTLeftStrategyNumber:
           flag = ! overRight2D(&next_nodespan, &queries[i]);
