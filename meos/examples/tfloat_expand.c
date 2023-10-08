9be5676b8ffb04effbcec7cd595c2447cc959196
Esteban Zimanyi <ezimanyi@ulb.ac.be>
2023-10-08 18:24:15 +0200

    Improve MEOS examples

---
+++ meos/examples/tfloat_expand.c
@@ -0,0 +1,119 @@
+/*****************************************************************************
+ *
+ * This MobilityDB code is provided under The PostgreSQL License.
+ * Copyright (c) 2016-2023, Université libre de Bruxelles and MobilityDB
+ * contributors
+ *
+ * MobilityDB includes portions of PostGIS version 3 source code released
+ * under the GNU General Public License (GPLv2 or later).
+ * Copyright (c) 2001-2023, PostGIS contributors
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose, without fee, and without a written
+ * agreement is hereby granted, provided that the above copyright notice and
+ * this paragraph and the following two paragraphs appear in all copies.
+ *
+ * IN NO EVENT SHALL UNIVERSITE LIBRE DE BRUXELLES BE LIABLE TO ANY PARTY FOR
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING
+ * LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION,
+ * EVEN IF UNIVERSITE LIBRE DE BRUXELLES HAS BEEN ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ *
+ * UNIVERSITE LIBRE DE BRUXELLES SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON
+ * AN "AS IS" BASIS, AND UNIVERSITE LIBRE DE BRUXELLES HAS NO OBLIGATIONS TO
+ * PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ *****************************************************************************/
+
+/**
+ * @brief A simple program that generates a given number of tfloat instants,
+ * appends the instant into a sequence at each generation, and outputs the
+ * final sequence at the end.
+ *
+ * The instants are generated so they are not redundant, that is, all input
+ * instants will appear in the final sequence. A compiler option allows to
+ * either use expandable structures or to create a new sequence at every new
+ * instant generated.
+ *
+ * The program can be build as follows
+ * @code
+ * gcc -Wall -g -I/usr/local/include -o tfloat_expand tfloat_expand.c -L/usr/local/lib -lmeos
+ * @endcode
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <meos.h>
+/* The expandable functions are in the internal MEOS API */
+#include <meos_internal.h>
+
+/* Maximum number of instants */
+#define MAX_INSTANTS 500000
+/* Number of instants in a batch for printing a marker */
+#define NO_INSTANTS_BATCH 1000
+
+/* Main program */
+int main(void)
+{
+  /* Initialize MEOS */
+  meos_initialize(NULL, NULL);
+
+  /* Get start time */
+  clock_t tm;
+  tm = clock();
+
+  /* Expandable sequence */
+  Temporal *seq = NULL;
+  /* Interval to add */
+  Interval *oneday = pg_interval_in("1 day", -1);
+  /* Iterator variable */
+  int i;
+
+  printf("Generating the instants (one '*' marker every %d instants)\n",
+    NO_INSTANTS_BATCH);
+
+  TimestampTz t = pg_timestamptz_in("1999-12-31", -1);
+  for (i = 0; i < MAX_INSTANTS; i++)
+  {
+    t = pg_timestamp_pl_interval(t, oneday);
+    /* Generate an instant that is kept upon normalization */
+    TInstant *inst = tfloatinst_make(i % 2 + 1, t);
+    /* Test whether it is the first instant read */
+    if (! seq)
+      /* Create an expandable temporal sequence that can store two instants
+       * and store the first instant */
+      seq = (Temporal *) tsequence_make_exp((const TInstant **) &inst, 1, 2,
+        true, true, LINEAR, false);
+    else
+      /* Append the instant to the temporal sequence so that if there is no
+       * more space the sequence automatically doubles its capacity */
+      seq = temporal_append_tinstant(seq, inst, 0.0, NULL, true);
+    free(inst);
+    if (i % NO_INSTANTS_BATCH == 0)
+    {
+      printf("*");
+      fflush(stdout);
+    }
+  }
+
+  /* Print information about the sequence */
+  printf("Number of instants: %d, Time-weighted average: %lf\n",
+    temporal_num_instants(seq), tnumber_twavg(seq));
+
+  /* Calculate the elapsed time */
+  tm = clock() - tm;
+  double time_taken = ((double) tm) / CLOCKS_PER_SEC;
+  printf("The program took %f seconds to execute\n", time_taken);
+  printf("Accumulating the instants and constructing the sequence at the end\n");
+
+  /* Free memory */
+  free(seq);
+
+  /* Finalize MEOS */
+  meos_finalize();
+
+  return 0;
+}
