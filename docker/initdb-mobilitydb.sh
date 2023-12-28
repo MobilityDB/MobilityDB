899876bf19899cf3e6b3c874183a9bfd51a0e97c
Maxime Schoemans <maxime.schoemans@ulb.ac.be>
2023-12-28 15:41:54 +0100

    Fix docker action

--- docker/initdb-mobilitydb.sh
+++ docker/initdb-mobilitydb.sh
@@ -12,5 +12,5 @@ set -e
 # Create the 'mobilitydb' extension in the mobilitydb database
 echo "Loading MobilityDB extension into mobilitydb"
 psql --user="$POSTGRES_USER" --dbname="mobilitydb" <<- 'EOSQL'
-	CREATE EXTENSION IF NOT EXISTS mobilitydb CASCADE;
+	CREATE EXTENSION IF NOT EXISTS postgis CASCADE;
 EOSQL
