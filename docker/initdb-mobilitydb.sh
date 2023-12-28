1681af9afe7c3fd5f70884c650a0b71bf81aa450
Maxime Schoemans <maxime.schoemans@ulb.ac.be>
2023-12-28 15:50:21 +0100

    Fix docker action

--- docker/initdb-mobilitydb.sh
+++ docker/initdb-mobilitydb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+set -e
+
 # Install MobilityDB
 mkdir /github/workspace/build
 cd /github/workspace/build
 cmake .. && make -j$(nproc) && make install
 
 echo "shared_preload_libraries = 'postgis-3.so'" >> $PGDATA/postgresql.conf
-
-set -e
 
 # Create the 'mobilitydb' extension in the mobilitydb database
 echo "Loading MobilityDB extension into mobilitydb"
