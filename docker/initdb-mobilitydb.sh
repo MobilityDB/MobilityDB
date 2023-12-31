29956bb14bd377a8096baf07a94e584803cfb997
Maxime Schoemans <maxime.schoemans@ulb.ac.be>
2023-12-31 19:23:13 +0100

    Rewrite initdb-mobilitydb using initdb-postgis template

--- docker/initdb-mobilitydb.sh
+++ docker/initdb-mobilitydb.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-echo "shared_preload_libraries = 'postgis-3.so'" >> "$PGDATA"/postgresql.conf
-
 set -e
 
-# Create the 'mobilitydb' extension in the mobilitydb database
-echo "Loading MobilityDB extension into mobilitydb"
-psql --user="$POSTGRES_USER" --dbname="mobilitydb" <<- 'EOSQL'
-	CREATE EXTENSION IF NOT EXISTS mobilitydb CASCADE;
+echo "shared_preload_libraries = 'postgis-3.so'" >> "$PGDATA"/postgresql.conf
+
+# Perform all actions as $POSTGRES_USER
+export PGUSER="$POSTGRES_USER"
+
+# Create the 'template_mobilitydb' template db
+"${psql[@]}" <<- 'EOSQL'
+CREATE DATABASE template_mobilitydb IS_TEMPLATE true;
 EOSQL
+
+# Load mobilitydb into both template_database and $POSTGRES_DB
+for DB in template_mobilitydb "$POSTGRES_DB"; do
+  echo "Loading mobilitydb extensions into $DB"
+  "${psql[@]}" --dbname="$DB" <<-'EOSQL'
+    CREATE EXTENSION IF NOT EXISTS mobilitydb CASCADE;
+EOSQL
+done
