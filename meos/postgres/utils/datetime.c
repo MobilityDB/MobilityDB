86c424859a4730c64826648b1c38ca98808837f2
Diviloper <vdiviloper@gmail.com>
2024-03-25 17:39:36 +0100

    Fix pointer conversion errors reported by CLang 15/16

--- meos/postgres/utils/datetime.c
+++ meos/postgres/utils/datetime.c
@@ -4400,7 +4400,7 @@ FetchDynamicTimeZone(TimeZoneAbbrevTable *tbl, const datetkn *tp)
      */
     if (dtza->tz == NULL)
     {
-      meos_error(ERROR, "time zone \"%s\" not recognized", dtza->zone);
+      meos_error(ERROR, MEOS_ERR_INVALID_ARG_VALUE, "time zone \"%s\" not recognized", dtza->zone);
       return NULL;
     }
   }
