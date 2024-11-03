44a7b78a0c14744c399564b8fa26d09e5d6cedeb
Esteban Zimanyi <esteban.zimanyi@ulb.be>
2024-11-03 21:29:45 +0100

    Improve npoint tests (#640)

--- mobilitydb/test/npoint/queries/096_tnpoint_spatialrels_tbl.test.sql
+++ mobilitydb/test/npoint/queries/096_tnpoint_spatialrels_tbl.test.sql
@@ -28,8 +28,8 @@
 -------------------------------------------------------------------------------
 
 -------------------------------------------------------------------------------
-set parallel_tuple_cost=0;
-set parallel_setup_cost=0;
+-- set parallel_tuple_cost=0;
+-- set parallel_setup_cost=0;
 -------------------------------------------------------------------------------
 -- Geometry rel tnpoint
  -------------------------------------------------------------------------------
@@ -93,8 +93,8 @@ SELECT COUNT(*) FROM tbl_tnpoint t1, tbl_tnpoint t2 WHERE aIntersects(t1.temp, t
 SELECT COUNT(*) FROM tbl_tnpoint t1, tbl_tnpoint t2 WHERE aDwithin(t1.temp, t2.temp, 0.01) AND t1.k%4 = 0 AND t2.k%4 = 0 AND tempSubtype(t1.temp) != 'SequenceSet';
 
 -------------------------------------------------------------------------------
-set parallel_tuple_cost=100;
-set parallel_setup_cost=100;
+-- set parallel_tuple_cost=100;
+-- set parallel_setup_cost=100;
 -------------------------------------------------------------------------------
 -- Test index support function for ever spatial relationships
 
