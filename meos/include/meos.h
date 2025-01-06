4f749feac228ab1872c053388590fa5c23a33059
Esteban Zimanyi <estebanzimanyi@gmail.com>
2025-01-06 21:49:49 +0100

    Add exponential function

--- meos/include/meos.h
+++ meos/include/meos.h
@@ -380,10 +380,6 @@ extern DateADT minus_date_int(DateADT d, int32 days);
 extern TimestampTz minus_timestamptz_interval(TimestampTz t, const Interval *interv);
 extern Interval *minus_timestamptz_timestamptz(TimestampTz t1, TimestampTz t2);
 extern Interval *mult_interval_double(const Interval *interv, double factor);
-
-extern double pg_ln(double arg1);
-extern double pg_log10(double arg1);
-
 extern char *text2cstring(const text *txt);
 extern int text_cmp(const text *txt1, const text *txt2);
 extern text *text_copy(const text *txt);
