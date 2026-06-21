6d1afd208172b0a780e93fce13a45ab06530ddc0
Esteban Zimanyi <estebanzimanyi@gmail.com>
2026-06-21 15:32:40 +0200

    Extend the pose base type and its set value accessors

--- meos/include/pose/pose.h
+++ meos/include/pose/pose.h
@@ -95,6 +95,10 @@ extern Pose *pose_parse(const char **str, bool end);
 
 extern Datum datum_pose_point(Datum pose);
 extern Datum datum_pose_rotation(Datum pose);
+extern Datum datum_pose_yaw(Datum pose);
+extern Datum datum_pose_pitch(Datum pose);
+extern Datum datum_pose_roll(Datum pose);
+extern Datum datum_pose_apply_geo(Datum pose, Datum body);
 
 /* Transformation functions */
 
