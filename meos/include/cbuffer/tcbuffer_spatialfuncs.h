a23ee60b452f52b4d11faf71c1982e49320bd88e
Esteban Zimanyi <estebanzimanyi@gmail.com>
2026-06-21 16:25:32 +0200

    Add the temporal circular buffer spatial functions

--- meos/include/cbuffer/tcbuffer_spatialfuncs.h
+++ meos/include/cbuffer/tcbuffer_spatialfuncs.h
@@ -45,8 +45,10 @@
 /* Traversed area functions */
 
 extern GSERIALIZED *tcbufferinst_traversed_area(const TInstant *inst);
-extern GSERIALIZED *tcbufferseq_traversed_area(const TSequence *seq);
-extern GSERIALIZED *tcbufferseqset_traversed_area(const TSequenceSet *ss);
+extern GSERIALIZED *tcbufferseq_traversed_area(const TSequence *seq,
+  bool unary_union);
+extern GSERIALIZED *tcbufferseqset_traversed_area(const TSequenceSet *ss,
+  bool unary_union);
 extern GSERIALIZED *tcbuffersegm_traversed_area(const TInstant *inst1,
   const TInstant *inst2);
 
