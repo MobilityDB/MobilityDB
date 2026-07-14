6c24201711730fb72c20d6e5d437d3fcfbbe8e68
Esteban Zimanyi <estebanzimanyi@gmail.com>
2026-07-14 15:43:30 +0200

    Remove Arrow support

--- mobilitydb/src/temporal/temporal.c
+++ mobilitydb/src/temporal/temporal.c
@@ -46,9 +46,6 @@
 #include <utils/timestamp.h>
 /* MEOS */
 #include <meos.h>
-#if ARROW
-  #include <meos_arrow.h>
-#endif
 #if RGEO
   #include <meos_rgeo.h>
 #endif
@@ -1642,25 +1639,6 @@ Temporal_round(PG_FUNCTION_ARGS)
   PG_FREE_IF_COPY(temp, 0);
   PG_RETURN_TEMPORAL_P(result);
 }
-
-#if ARROW
-PGDLLEXPORT Datum Temporal_arrow_roundtrip(PG_FUNCTION_ARGS);
-PG_FUNCTION_INFO_V1(Temporal_arrow_roundtrip);
-/**
- * @ingroup mobilitydb_temporal_transf
- * @brief Round-trip a temporal value through the Arrow C Data Interface,
- * returning the reconstructed value
- * @sqlfn arrowRoundtrip()
- */
-Datum
-Temporal_arrow_roundtrip(PG_FUNCTION_ARGS)
-{
-  Temporal *temp = PG_GETARG_TEMPORAL_P(0);
-  Temporal *result = meos_temporal_arrow_roundtrip(temp);
-  PG_FREE_IF_COPY(temp, 0);
-  PG_RETURN_TEMPORAL_P(result);
-}
-#endif /* ARROW */
 
 PGDLLEXPORT Datum Temporalarr_round(PG_FUNCTION_ARGS);
 PG_FUNCTION_INFO_V1(Temporalarr_round);
